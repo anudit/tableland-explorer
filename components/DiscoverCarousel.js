323dcf1720e0c7522d81ca8f043b4e7cc702d759
Anudit Nagar <nagaranudit@gmail.com>
2023-09-22 16:26:06 +0530

    Update Deps

--- components/DiscoverCarousel.js
+++ components/DiscoverCarousel.js
@@ -1,14 +1,14 @@
+import { Box, Flex, Image, Text, useColorMode, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
-import { useMediaQuery, Box, Flex, Image, Text, useColorMode } from "@chakra-ui/react";
 import Balancer from 'react-wrap-balancer';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from "swiper";
 import 'swiper/css';
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 
-export default function DiscoverCarousel({projects: slides}){
+export default function DiscoverCarousel({ projects: slides }) {
 
   const [isLargerThanMd] = useMediaQuery('(min-width: 1280px)')
 
@@ -21,28 +21,28 @@ export default function DiscoverCarousel({projects: slides}){
       mt={8}
     >
       <Flex w="full" pos="relative" overflow="hidden">
-      <Swiper effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={isLargerThanMd ? 3 : 1}
-        loopedSlides={2}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        index
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]} 
-        style={{
-          paddingTop: '50px',
-          paddingBottom: '50px'
-        }}
-       >
-        {
-            slides.slice(0).sort((a, b)=>{
+        <Swiper effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={isLargerThanMd ? 3 : 1}
+          loopedSlides={2}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          index
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          style={{
+            paddingTop: '50px',
+            paddingBottom: '50px'
+          }}
+        >
+          {
+            slides.slice(0).sort((a, b) => {
               return a?.name.toLowerCase().charCodeAt(0) - b?.name.toLowerCase().charCodeAt(0)
             }).map((slide, sid) => (
               <SwiperSlide key={`slide-${sid}`}>
@@ -56,46 +56,46 @@ export default function DiscoverCarousel({projects: slides}){
 }
 
 
-const ProjectSlide = ({slide}) => {
+const ProjectSlide = ({ slide }) => {
   const { colorMode } = useColorMode();
-  return(
+  return (
     <Box
       flexDirection="column" w="100%" shadow="md" flex="none" alignItems="center"
-      borderRadius='20px' 
+      borderRadius='20px'
       height="auto"
       position='relative'
       overflow="hidden"
       maxWidth="800px"
       _hover={{
         '> img': {
-            transform: 'scale(1.1)',
-            transition: '0.3s ease'
+          transform: 'scale(1.1)',
+          transition: '0.3s ease'
         }
       }}
     >
       <Image
-          src={slide.poster}
-          borderRadius='20px'
-          alt={`Poster Image`}
+        src={slide.poster}
+        borderRadius='20px'
+        alt={`Poster Image`}
       />
-      <Flex 
-          className='overlay-text'
-          h="-webkit-fill-available"
-          position="absolute"
-          flexDirection='column'
-          alignItems='flex-start' 
-          justifyContent='space-between'
-          top={0}
-          w="full"
-          background={colorMode === 'light' ? 'none' : 'linear-gradient(#ffffff00, #000000c9 90%)'}
+      <Flex
+        className='overlay-text'
+        h="-webkit-fill-available"
+        position="absolute"
+        flexDirection='column'
+        alignItems='flex-start'
+        justifyContent='space-between'
+        top={0}
+        w="full"
+        background={colorMode === 'light' ? 'none' : 'linear-gradient(#ffffff00, #000000c9 90%)'}
       >
         <Flex direction='column-reverse' alignItems='center' align="center" w="full" h="full" mb={4} color="white">
-            <Balancer align="center" >
-              <Text fontSize={{base:'sm' ,md:'md'}} width="100%">
+          <Balancer align="center" >
+            <Text fontSize={{ base: 'sm', md: 'md' }} width="100%">
               {slide.description}
-              </Text>
-            </Balancer>
-            <Text fontSize={{base:'xl' ,md:'2xl'}} fontWeight={600}>{slide.name}</Text>
+            </Text>
+          </Balancer>
+          <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight={600}>{slide.name}</Text>
         </Flex>
       </Flex>
     </Box>
